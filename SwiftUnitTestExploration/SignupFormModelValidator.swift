e5346234c186b2d7d55bc3b3963b25a432a1bc07
Ayodeji <ayodejiayankola@gmail.com>
2021-11-01 11:09:26 +0100

    test for too short name

--- SwiftUnitTestExploration/SignupFormModelValidator.swift
+++ SwiftUnitTestExploration/SignupFormModelValidator.swift
@@ -9,17 +9,17 @@ import Foundation
 
 class SignupFormModelValidator {
     
+//    func isFirstNameValid(firstName: String) -> Bool {
+//        var returnValue =  true
+//        if firstName.isEmpty {
+//            returnValue = false
+//        }
+//        return returnValue
+//    }
+    
     func isFirstNameValid(firstName: String) -> Bool {
         var returnValue =  true
-        if firstName.isEmpty {
-            returnValue = false
-        }
-        return returnValue
-    }
-    
-    func isFirstNameNotShort(firstName: String) -> Bool {
-        var returnValue =  true
-        if firstName.count < 3 {
+        if firstName.count <  2  {
             returnValue = false
         }
         return returnValue
--- SwiftUnitTestExplorationTests/SignupFormModelValidatorTests.swift
+++ SwiftUnitTestExplorationTests/SignupFormModelValidatorTests.swift
@@ -9,30 +9,38 @@ import XCTest
 @testable import SwiftUnitTestExploration
 
 class SignupFormModelValidatorTests: XCTestCase {
-
+    
+    var sut: SignupFormModelValidator!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = SignupFormModelValidator()
+        
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
     func testSignupFormModelValidator_WhenValidFirstNameIsProvided_ShouldReturnTrue() {
         // Create a structure
         
         // Arrange - system under test - sut
-        let sut = SignupFormModelValidator()
         // Act
        let isFirstNameValid =  sut.isFirstNameValid(firstName: "Ayodeji")
         
         
         // Assert
         XCTAssertTrue(isFirstNameValid, "The isFirstNameValid() Should have returned TRUE for a valid first name but returned FALSE")
-        
-        let isFirstNameNotShort = sut.isFirstNameNotShort(firstName: "Deji")
-      
-        XCTAssertTrue(isFirstNameNotShort, "The isFirstNameNotShort() Should have returned TRUE for a valid first name but returned FALSE")
+    }
+    
+    
+    func testSignupFormModelValidator_WhenTooShortFirstNameIsProvided_ShouldReturnFalse(){
+        //Arrange
+        //Act
+        let isFirstNameValid = sut.isFirstNameValid(firstName: "A")
+        //Assert
+        XCTAssertFalse(isFirstNameValid, "he isFirstNameValid() Should have returned FALSE for a first name that is shorter than 2 characters but it has returned TRUE")
     }
 
 }
